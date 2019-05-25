6e60f265823448085b1f5bfae430cd8ca64e7b63
zerozww <2457244971@qq.com>
2019-05-25 18:10:09 +0800

    bookstore

---
+++ src/main/java/com/springmvc/bookstore/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.springmvc.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
